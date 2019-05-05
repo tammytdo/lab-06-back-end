e80030c47276312a92894c18805b13b82b9ebf04
beck <rebecca@rebeccapeterson.us>
2019-05-05 10:08:17 -0700

    resubmitting lab6 with city explorer rendering lynnwood data stored in our local files

--- server.js
+++ server.js
@@ -12,34 +12,71 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-// what the server does
-//the route
-//request = data from query. example, from a front end query
-//can test in localhost:3000/location to verify
+// app.get('/location', (request, response) => {
+//   const testData = {
+//     "search_query": "seattle",
+//     "formatted_query": "Seattle, WA, USA",
+//     "latitude": "47.606210",
+//     "longitude": "-122.332071"
+//   };
+//   response.send(testData);
+// })
 app.get('/location', (request, response) => {
-  response.send( searchLatLng(request.query.data) );
+  //commenting out so that you can search on any city and see lynnwood data
+  // if(request.query.data !== 'lynnwood'){
+  //   response.status(500).send('Hi there, I only have the lynnwood data.')
+  // }
+  response.send(searchLatLng(request.query.data));
+});
+
+//first parameter is a route or endpoint
+app.get('/weather', (request, response) =>{
+  const finalArrayOfWeather = [];
+
+  const weatherJSON = require('./data/darksky.json');
+  const dailyWeather = weatherJSON.daily;
+  const dailyWeatherData = dailyWeather.data;
+  //dayObj is our raw data
+  dailyWeatherData.forEach(dayObj => {
+    finalArrayOfWeather.push(new DailyWeather(dayObj));
+  })
+  response.send(finalArrayOfWeather);
 })
+
 
 app.use('*', (request, response) => {
   response.send('Our server runs.');
 })
 
+/////////Helper Functions /////////////////////////////////
+function DailyWeather(rawDayObj) {
+  this.forecast = rawDayObj.summary;
+  this.time = new Date (rawDayObj.time * 1000).toString().slice(0, 15);
+}
+
+
 function searchLatLng(frontEndQuery) {
- // take the data from the front end, as the searched for location ('berlin')
+
+  //take the data from the front end as the searched for location
   const search_query = frontEndQuery;
 
- // Go out and get data, tomorrow
-  const testData = require('./data/geo.json'); // go get some other data
+  const testData = require('./data/geo.json'); //go get some other data
 
-  const long_name = testData.results[0].formatted_address;
-  const short_name = testData.results[0].short_name;
-  const types = testData.results[0].types;
+  const formatted_query = testData.results[0].formatted_address;
+  const results = testData.results;
+  const oneResult = results[0];
+  const geometry = oneResult.geometry;
+  const location = geometry.location;
+  const latitude = location.lat;
+  const longitude = location.lng;
 
-  const responseObject = { search_query, long_name, short_name, types };
+  // const latitude = testData.results[0].geometry.location.lat;
+  // const longitude = testData.results[0].geometry.location.lng;
+
+  const responseObject = { search_query, formatted_query, latitude, longitude };
 
   return responseObject;
-
-}
+};
 
 //server start
 app.listen(PORT, ()=> {
