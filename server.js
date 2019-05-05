5656ebf4ddab20e02b9fe8c1216a09c81bd746a8
beck <rebecca@rebeccapeterson.us>
2019-05-05 11:13:45 -0700

    update originalv4

--- server.js
+++ server.js
@@ -12,37 +12,71 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-// what the server does
-//the route
-//request = data from query. example, from a front end query
-//can test in localhost:3000/location to verify
+// app.get('/location', (request, response) => {
+//   const testData = {
+//     "search_query": "seattle",
+//     "formatted_query": "Seattle, WA, USA",
+//     "latitude": "47.606210",
+//     "longitude": "-122.332071"
+//   };
+//   response.send(testData);
+// })
+app.get('/location', (request, response) => {
+  //commenting out so that you can search on any city and see lynnwood data
+  // if(request.query.data !== 'lynnwood'){
+  //   response.status(500).send('Hi there, I only have the lynnwood data.')
+  // }
+  response.send(searchLatLng(request.query.data));
+});
 
-// Switched app.get from an anonymous function to a named callback. 
-app.get('/location', searchLatLng);
+//first parameter is a route or endpoint
+app.get('/weather', (request, response) =>{
+  const finalArrayOfWeather = [];
 
-// Standard response for when a route that does not exist is accessed. 
+  const weatherJSON = require('./data/darksky.json');
+  const dailyWeather = weatherJSON.daily;
+  const dailyWeatherData = dailyWeather.data;
+  //dayObj is our raw data
+  dailyWeatherData.forEach(dayObj => {
+    finalArrayOfWeather.push(new DailyWeather(dayObj));
+  })
+  response.send(finalArrayOfWeather);
+})
+
+
 app.use('*', (request, response) => {
   response.send('Our server runs.');
 })
 
-function searchLatLng(request, response) {
-  // take the data from the front end, as the searched for location ('berlin')
-  console.log('proof of life B');
-  const search_query = request.query.data;
+/////////Helper Functions /////////////////////////////////
+function DailyWeather(rawDayObj) {
+  this.forecast = rawDayObj.summary;
+  this.time = new Date (rawDayObj.time * 1000).toString().slice(0, 15);
+}
 
-  // Go out and get data, tomorrow
-  const testData = require('./data/geo.json'); // go get some other data
+
+function searchLatLng(frontEndQuery) {
+
+  //take the data from the front end as the searched for location
+  const search_query = frontEndQuery;
+
+  const testData = require('./data/geo.json'); //go get some other data
 
   const formatted_query = testData.results[0].formatted_address;
-  const latitude = testData.results[0].geometry.location.lat;
-  const longitude = testData.results[0].geometry.location.lng;
+  const results = testData.results;
+  const oneResult = results[0];
+  const geometry = oneResult.geometry;
+  const location = geometry.location;
+  const latitude = location.lat;
+  const longitude = location.lng;
+
+  // const latitude = testData.results[0].geometry.location.lat;
+  // const longitude = testData.results[0].geometry.location.lng;
 
   const responseObject = { search_query, formatted_query, latitude, longitude };
-  console.log(responseObject);
 
-
-  response.send(responseObject);
-}
+  return responseObject;
+};
 
 //server start
 app.listen(PORT, ()=> {
